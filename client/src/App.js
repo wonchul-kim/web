2e7fd9efe9ba20c385ce575bfba7faa5862267c4
wonchul <onedang22@gmail.com>
2022-01-07 13:37:58 +0900

    trying to add appbar

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,16 @@ import { withStyles, makeStyles } from '@mui/styles';
 import { CircularProgress } from '@mui/material';
 import CusotmersAdd from './components/CustomersAdd';
 
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+
 
 const styles = theme => ({
   root: {
@@ -156,7 +166,7 @@ class App extends React.Component {
           </Table>
         </Paper>
         <CusotmersAdd stateRefresh={this.stateRefresh} />
-        </div>
+      </div>
     );
   }
 }
